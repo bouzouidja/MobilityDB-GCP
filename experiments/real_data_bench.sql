6c2bb4b9312c922bacf7d11e02c8ff2af5c30a5f
bouzouidja <bouzouidjasidahmed@gmail.com>
2023-08-21 09:19:34 +0200

    delete real data bench script

--- experiments/real_data_bench.sql
+++
@@ -1,6 +0,0 @@
-SET datestyle = dmy;
-\COPY AISInput(T, TypeOfMobile, MMSI, Latitude, Longitude, NavigationalStatus,
-    ROT, SOG, COG, Heading, IMO, CallSign, Name, ShipType, CargoType, Width, Length,
-    TypeOfPositionFixingDevice, Draught, Destination, ETA, DataSourceType,
-    SizeA, SizeB, SizeC, SizeD)
-  FROM './aisdk-2023-03-07.csv' DELIMITER ',' CSV HEADER;
